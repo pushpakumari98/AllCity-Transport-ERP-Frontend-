3ae7d601d806deae5d895f2f39ad7f56f2254610
pushpakumari98 <pushpa20052002kumari@gmail.com>
2026-01-25 21:20:33 +0530

    hu

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,27 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { environment } from './environments/environment';
 
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
+import { AuthInterceptor } from './app/shared/interceptors/auth.interceptor';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, AppRoutingModule,HttpClientModule),
-    provideAnimations()],
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule, HttpClientModule),
+    provideAnimations(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
 
 }).catch((err) => console.error(err));
