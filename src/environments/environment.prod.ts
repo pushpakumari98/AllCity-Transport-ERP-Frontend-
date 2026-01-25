c77ee26f40429feff43ace7c6b097fbf6bb50104
pushpakumari98 <pushpa20052002kumari@gmail.com>
2026-01-25 15:33:01 +0530

    uy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 import packageInfo from '../../package.json';
 
 export const environment = {
-  appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'https://api.allcity-transport.com/api'
-
+  apiUrl: 'https://allcity-transport-erp.onrender.com'
 };
+
+
